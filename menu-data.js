9fdb0182444874b1e58adf0bcea57943de70149b
jaranramen <jaranramen@gmail.com>
2026-07-16 19:13:06 +0330

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -398,14 +398,14 @@ window.MENU = {
         key: "desserts",
         icon: "🍮",
         items: [
-        //   {
-        //     id: "dessert-creme-brulee",
-        //     name: { en: "Crème Brûlée", fa: "کرم بروله" },
-        //     descShort: { en: "Rich custard dessert.", fa: "دسر کاستارد غنی." },
-        //     descLong: { en: "Silky custard with caramelized sugar top.", fa: "کاستارد لطیف با رویه کاراملی." },
-        //     price: 150000,
-        //     // status: "comingSoon" // ← これ
-        //   },        
+          // {
+          //   id: "dessert-creme-brulee",
+          //   name: { en: "Crème Brûlée", fa: "کرم بروله" },
+          //   descShort: { en: "Rich custard dessert.", fa: "دسر کاستارد غنی." },
+          //   descLong: { en: "Silky custard with caramelized sugar top.", fa: "کاستارد لطیف با رویه کاراملی." },
+          //   price: 250000,
+          //   // status: "comingSoon" // ← これ
+          // },        
         
           {
             id: "dessert-mochi-ice",
